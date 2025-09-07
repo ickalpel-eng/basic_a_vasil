5293211d79ac0c727e528705717582b3041a59d9
alexandr <i.ckalpel@gmail.com>
2025-09-07 18:43:05 +0300

    homework 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,30 @@
-print("Привет мир")
+a = 7
+b = 8.34
+str_1 = "legko v IT"
+# b = a
+# print(a,str_1,b)
+
+# a,b,c = 0, 1, 2
+# print(a,b,c)
+
+# a, b = 8, 10
+# a, b = b, a
+# print(a)
+# print(b)
+
+print(type(a))
+print(type(str_1))
+print(type(b))
+
+# message = "Привет, друг"
+# count = 0
+# Count = 0
+
+a = c = 4
+b, d = "Какая-то строка", "Вторая строка"
+
+print(id(a))
+print(id(c))
+print(id(b))
+print(id(d))
+
