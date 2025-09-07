714911805b50bdd7115ffba6f82c6381e51c8a90
alexandr <i.ckalpel@gmail.com>
2025-09-07 19:28:35 +0300

    homework 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,30 +1,58 @@
+name = "Alexandr"
+age = 24
+height = 64
+print("Имя - ", name)
+print("Возраст -", age)
+print("Вес - ", height)
+x = 10
+print(type(x))
+x = 25.5
+print(type(x))
+x = "Python"
+print(type(x))
+print(x)
 a = 7
-b = 8.34
-str_1 = "legko v IT"
-# b = a
-# print(a,str_1,b)
-
-# a,b,c = 0, 1, 2
-# print(a,b,c)
-
-# a, b = 8, 10
-# a, b = b, a
-# print(a)
-# print(b)
-
-print(type(a))
-print(type(str_1))
-print(type(b))
-
-# message = "Привет, друг"
-# count = 0
-# Count = 0
-
-a = c = 4
-b, d = "Какая-то строка", "Вторая строка"
-
-print(id(a))
-print(id(c))
-print(id(b))
-print(id(d))
-
+b = a
+print(a)
+print(b)
+a = 10
+print(b, "Значение не изменилось, так как на стр 18 b зафиксировало значение a (на тот момент 7), а мы не меняли после этого значение b")
+x, y, z = 100,100,100
+print(x,y,z)
+print(id(x))
+print(id(y))
+print(id(z))
+x, y, z = 100, 101, 102
+print(x,y,z)
+print(id(x))
+print(id(y))
+print(id(z))
+print("Разные")
+a = 5
+b = 10
+a,b = 10, 5
+print(a,b)
+import keyword
+print("Переменные нельзя так называть, так как они специально зарезервированы")
+print(keyword.kwlist)
+var1 = 42
+var2 = 3.14
+var3 = "Hello"
+print(type(var1))
+print(type(var2))
+print(type(var3))
+var1 = "Проверка"
+print(type(var1))
+lastname = "Ivanov"
+dog_name = "Sharik"
+author = "Esenin"
+Float = 8.14
+count = 0
+print(lastname,type(lastname))
+print(dog_name,type(dog_name))
+print(author,type(author))
+print(Float,type(Float))
+print(count,type(count))
+знач = 10
+print(знач)
+print("Да, работает")
